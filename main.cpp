4d469aec4fe48e6d563920eca9300e6698a78eda
Gustavo Ferrufino <gustavo.fdlf@gmail.com>
2015-10-20 22:12:38 -0500

    meh

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  graficas
+//
+//  Created by Gustavo Ferrufino on 10/20/15.
+//  Copyright (c) 2015 Gustavo Ferrufino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
